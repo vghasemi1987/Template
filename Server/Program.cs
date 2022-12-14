c52bed3ebefd270fb47a7e437090b9ea7552e7f5
Vahid Ghasemi <v.ghasemi1987@hotmail.com>
2022-12-14 08:53:48 +0330

    Modify

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,15 +1,45 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Server
 {
 	internal class Program
 	{
-		public static void Main( string [] args )
+		public static void Main()
 		{
-			WebApplicationBuilder? builder = WebApplication.CreateBuilder( args );
+
+			WebApplicationOptions? webApplicationOptions =
+				new Microsoft.AspNetCore.Builder.WebApplicationOptions
+				{
+					EnvironmentName =
+					Microsoft.Extensions.Hosting.Environments.Development ,
+				};
+
+			WebApplicationBuilder? builder =
+				WebApplication.CreateBuilder( options: webApplicationOptions );
+
+
+			builder.Services.AddRazorPages( );
+
 			WebApplication? app = builder.Build( );
 
-			app.MapGet( pattern: "/" , () => "Hello World!" );
+			if ( app.Environment.IsDevelopment( ) )
+			{
+				app.UseDeveloperExceptionPage( );
+			}
+			else
+			{
+				app.UseExceptionHandler( errorHandlingPath: "/Errors/Error" );
+				app.UseHsts( );
+			}
+			app.UseHttpsRedirection( );
+
+			app.UseStaticFiles( );
+
+			app.UseRouting( );
+
+			app.MapRazorPages( );
 
 			app.Run( );
 		}
